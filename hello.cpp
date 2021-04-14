9c7c8a9f2e4defb14103bca8f9566f703cf695f7
NoobLord47 <82526146+NoobLord47@users.noreply.github.com>
2021-04-15 00:19:46 +0600

    some code were written

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    for(int i=a;i<b;i++){
+        cout<<"is this life or hell"<<endl;
+    }
+    cout<<"all i want to get rid of this life"<<endl;
+}
